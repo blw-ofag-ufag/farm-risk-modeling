78c0cf5e1ff6fccea797bd535d0da400a1e19dd1
Damian-Oswald <damian.oswald@protonmail.com>
2025-09-04 09:20:22 +0200

    Aesthetic changes in hepers.R

--- scripts/helpers.R
+++ scripts/helpers.R
@@ -6,6 +6,7 @@
 #'               scripts.
 #' =============================================================================
 
+library(magrittr)
 
 #' =============================================================================
 #' DROP FACTOR VARIABLES WHERA ALL FACTORS ARE THE SAME
@@ -29,37 +30,39 @@ drop_unique_factors <- function(data) {
 #' grouping variables), finally returning the predictor columns (without the
 #' farm ID) in two separate data frames.
 #'
-#' @param data           The dataset to be split.
-#' @param theme          The theme to be selected.
-#' @param predictors     The predictos to be selected.
-#' @param training_size  Proportion (from 0 to 1) of the training data set.
+#' @param data            The dataset to be split.
+#' @param theme           The theme to be selected.
+#' @param predictors      The predictos to be selected.
+#' @param training_size   Proportion (from 0 to 1) of the data that ought to be
+#'                        assigned to the training data set.
 #' =============================================================================
 
 data_split <- function(data, theme, predictors, training_size = 0.8) {
-  #' Subset the data by theme, and select "farm", predictors, and any groups
+
+  # subset the data by theme, and select "farm", predictors, and any groups
   df <- data[
     i    = data[["theme"]] == theme,
     j    = base::unique(c("farm", predictors)),
     drop = FALSE
   ] %>%
 
-    #' Drop any rows with missing data
+    # Drop any rows with missing data
     stats::na.omit() %>%
 
-    #' Drop any columns that have fewer than 2 unique values
-    #' (farm will usually have >1, so it stays).
+    # Drop any columns that have fewer than 2 unique values
+    # (farm will usually have >1, so it stays).
     drop_unique_factors()
 
-  #' select only unique farms (one farm belongs to test or train, not both)
+  # select only unique farms (one farm belongs to test or train, not both)
   x <- base::unique(df$farm)
 
-  #' sample some train farms
+  # sample some train farms
   train_farms <- base::sample(x, training_size * base::length(x))
 
-  #' save indices of train farms in the `df` data frame
+  # save indices of train farms in the `df` data frame
   i <- df[["farm"]] %in% x[x %in% train_farms]
 
-  #' return two data sets, one train, one test, one original and indices
+  # return two data sets, one train, one test, one original and indices
   list(
     train = df[i,  , drop = FALSE],
     test  = df[!i, , drop = FALSE],
@@ -73,12 +76,13 @@ data_split <- function(data, theme, predictors, training_size = 0.8) {
 #' GET A VARIABLE'S TITLE FROM FRICTIONLESS METADATA
 #' -----------------------------------------------------------------------------
 #'
-#' @param variable   The 'name' of the field you want the title for.
-#' @param metadata   An R list object read from a Frictionless YAML file.
-#' @param lang       The desired language for the title (e.g., "en", "de").
-#' @param unit       Should the unit be concatenated to the returned title?
+#' @param variable        The 'name' of the field you want the title for.
+#' @param metadata        An R list object read from a Frictionless YAML file.
+#' @param lang            The desired language for the title (e.g., "en", "de").
+#' @param unit            Should the unit be concatenated to the returned title?
 #'
-#' @return The title string, or NULL if not found.
+#' @return The constructed title string in a given language, or NULL if con-
+#'         structing the title string failed.
 #' =============================================================================
 
 get_title <- function(variable, metadata, lang = "en", unit = FALSE) {
